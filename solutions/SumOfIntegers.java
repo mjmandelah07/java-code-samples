0a091b69f173102e5c27f67b38ce11cdc9a8c956
mjmandelah07 <aramidemojisolagrace@gmail.com>
2024-05-02 11:14:21 +0100

    Sum of integers

---
+++ solutions/SumOfIntegers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfIntegers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user to enter a positive integer N
+        System.out.println("Enter a positive number N: ");
+        int N = input.nextInt();
+
+        // Calculate the sum of integers from 1 to N
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+
+        // Display the sum to user
+        System.out.println("The sum of integers from 1 to " + N + " is: " + sum);
+    }
+
+}
