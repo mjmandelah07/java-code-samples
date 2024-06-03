d080d88e8da1f07aa30d23de268feadc60b4a0a3
mjmandelah07 <aramidemojisolagrace@gmail.com>
2024-06-03 18:05:30 +0100

    solutions to java problems

---
+++ solutions/ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String originalStr = "Hello";
+        StringBuilder reversedStr = new StringBuilder();
+        System.out.println("Original string: " + originalStr);
+
+        for (int i = 0; i < originalStr.length(); i++){
+            reversedStr.insert(0, originalStr.charAt(i));
+        }
+
+        System.out.println("Reversed string: "+ reversedStr);
+    }
+}
---
+++ solutions/SumOfArray.java
@@ -0,0 +1,13 @@
+public class SumOfArray {
+    public static void main(String[] args) {
+        int[] myArray = {1, 5, 10, 25};
+        int sum = 0;
+        int i;
+
+        for (i = 0; i < myArray.length; i++) {
+            sum+=myArray[i];
+        }
+
+        System.out.println("The sum is: " + sum);
+    }
+}
